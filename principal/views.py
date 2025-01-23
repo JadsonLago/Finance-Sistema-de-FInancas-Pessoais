f0cf11ae25bf184be8b88869cd43d9c17cf0c5e7
Jadson <oi@evotax.com>
2025-01-22 21:40:31 -0300

    configuracao de urls

--- principal/views.py
+++ principal/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    message = 'Hello World'
+    context = {'message': message}
+    return render(request, 'principal/home.html', context)
